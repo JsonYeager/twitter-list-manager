d3e80f7a2de98eb6749732183c51f59c27ce4288
Jason Yeager <jason.yeager1@gmail.com>
2016-03-22 15:34:15 -0700

    wire up oAuth and return JSON from Twitter is now successful

--- js/routes.js
+++ js/routes.js
@@ -1,18 +1,14 @@
 // Configs
-twitterListManagerApp.config(function($routeProvider){
-	
-	$routeProvider
-	
-	.when('/',{
-		controller: 'homeController',
-		templateUrl: 'pages/home.htm'
-	})	
-	.when('/timeline',{
-		controller: 'timeLineController',
-		templateUrl: 'pages/timeline.htm'
-	})	
-	.when('/timeline/:action',{
-		controller: 'timeLineController',
-		templateUrl: 'pages/timeline.htm'
-	});	
+twitterListManagerApp.config(function($routeProvider) {
+
+    $routeProvider.when('/', {
+        controller: 'homeController',
+        templateUrl: 'pages/home.htm'
+    }).when('/timeline', {
+        controller: 'homeController',
+        templateUrl: 'pages/timeline.htm'
+    }).when('/timeline/:action', {
+        controller: 'homeController',
+        templateUrl: 'pages/timeline.htm'
+    });
 });
